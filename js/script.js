8a44eaf31dba05ad0132ae2409437f8b9a7edb7e
AgentC dev <brian@agentc.io>
2014-05-29 19:07:28 -0400

    Start of Jekyllization

---
+++ js/script.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+	
+	var headerHeight	= $('#site-header').height(),
+		twitterURL		= 'http://api.twitter.com/1/statuses/user_timeline.json?callback=?&screen_name=cinemaandspice&count=100';
+	
+	var scrollorama = $.scrollorama({
+        blocks:'.scrollblock'
+    });
+
+	if($('.scrollblock').length){
+		scrollorama.animate('.transparent-background',{ duration: 160, property:'opacity', start:1,end:0});
+		scrollorama.animate('#mission-statement',{ duration: 160, property:'opacity', start:1,end:0});
+	}
+	
+	function sizeFullScreenImages(){
+		var winHeight 		= $(window).height(),
+			fullWidthHeight = (winHeight - 60),
+			missionPosition	= (fullWidthHeight - $('#mission-statement').height() + headerHeight) * .5;					
+			
+		$('#introduction .full-width-image').css('height', fullWidthHeight);
+		$('#mission-statement').css('top', missionPosition);
+	}
+	
+	sizeFullScreenImages();
+	
+	$(window).resize(function() {
+		sizeFullScreenImages();
+	});
+	
+	$('a.scrollTo').click(function(event) {
+		event.preventDefault ? event.preventDefault() : event.returnValue = false;
+		$url = $(this).attr('href');
+		$('html:not(:animated), body:not(:animated)').animate({
+			scrollTop: $($url).offset().top - headerHeight
+		}, 600, 'swing', function() {
+
+			if (this.nodeName === "BODY") {
+				return;
+			}
+		});
+	});
+	
+	// Fetch Tweets and Add to Page
+	
+	function ajaxLoadContent() {
+
+		$.ajax({
+			type: "GET",
+			url: "tweets.html",
+			dataType: "html",
+			success: function(out){
+				
+				var result = $(out);
+				//var pageContent = $(out).find("#content");
+
+				$('#tweets').append(result.fadeIn(300, function() {
+				
+				}));
+				
+				
+			}
+       });
+	}
+	
+	ajaxLoadContent();
+	
+});
---
+++ js/slideshow.js
@@ -0,0 +1,142 @@
+$(document).ready(function(){
+
+  $('.slideshow').each(function() {
+
+  var index = 0,                          // current location in slideshow
+      $slides = $('.slide'),              // array of slide elements
+      slide = $slides[index],              // currently focused slide
+      video = $(slide).find('video')[0],  // current video element
+      videos = [],                        // array of video elements
+      queued,                             // video to be played next
+      is_touch = Modernizr.touch,
+      slideshowID = $(this).attr('id'),
+      flipsnap = new Flipsnap(this, {transitionDuration: 600});
+
+  //mixpanel.track('YiR/2013 Initialize');
+
+  flipsnap.element.addEventListener('fspointmove', function(e) {
+    $('.shifty').removeClass('show-menu');
+    $slides.removeClass('active');
+    index = e.newPoint; // customized flipsnap event to provide this
+    slide = $slides[index];
+    
+    $(slide).addClass('active');
+    
+    $('body').removeClass().addClass( $($slides[index]).attr('id') ).addClass( $($slides[index]).attr('data-color') );
+    // if( ($('.js-slide-previous').css('display') !== 'none') && ($('.js-slide-next').css('display') !== 'none') ){
+    if ($('.js-slide-previous').is(':visible') || $('.js-slide-next').is(':visible')){
+      if (!flipsnap.hasNext()) {
+        $('.js-slide-next').addClass('hide');
+      } else if (!$('.js-slide-next').is(':visible')){
+        $('.js-slide-next').removeClass('hide');
+      }
+      if (!flipsnap.hasPrev()) {
+        $('.js-slide-previous').addClass('hide');
+        $('.js-coach-mark').removeClass('hide').addClass('table-cell');
+      } else if (!$('.js-slide-previous').is(':visible')){
+        $('.js-slide-previous').removeClass('hide');
+        $('.js-coach-mark').removeClass('table-cell').addClass('hide');
+      } else if ($('.js-coach-mark').is(':visible')){
+        $('.js-coach-mark').removeClass('table-cell').addClass('hide');
+      }
+    }
+  }, false);
+
+
+  $('.js-slide-btn').click(function(e){
+    e.preventDefault();
+    index = $($(this).attr('href')).index('.slide');
+    flipsnap.moveToPoint(index);
+  });
+
+  $('.js-slide-next').click(function(){
+    closeModals();
+    flipsnap.toNext();
+    //mixpanel.track('YiR/2013 Next', { 'keypress' : false });
+  });
+
+  $('.js-slide-previous').click(function(){
+    closeModals();
+    flipsnap.toPrev();
+    //mixpanel.track('YiR/2013 Previous', { 'keypress' : false });
+  });
+
+  if (!flipsnap.hasPrev()) {
+    $('.js-slide-previous').addClass('hide');
+  }
+
+  function toggleMenu(){
+    closeModals();
+    if ($('.shifty').hasClass('show-menu')) {
+      $('.shifty').removeClass('show-menu');
+    } else {
+      $('.shifty').addClass('show-menu');
+    }
+  }
+
+  $('.js-shifty-toggle').click(function(){
+    toggleMenu();
+    //mixpanel.track('YiR/2013 View All');
+  });
+
+  function closeModals(){
+    $('.js-modal').fadeOut();
+    $('.js-modal').find('.fluid-width-video-wrapper').remove();
+  }
+
+  $('.js-show-modal').click(function(){
+    var $modal = $(this).parents('.slide').find('.js-modal'),
+        $iframe = $('<iframe>'),
+        src = $modal.attr('data-src'),
+        container = $modal.find('.js-embed-wrap')[0];
+    $modal.show();
+    $iframe.attr('src', src).attr('frameborder', 0);
+    $(container).append($iframe);
+    $(container).fitVids();
+  });
+
+  $('.js-hide-modal').click(function(){
+    closeModals();
+  });
+
+  $(document).keydown(function(e){
+    if (e.keyCode === 37) {
+      closeModals();
+      flipsnap.toPrev();
+      //mixpanel.track('YiR/2013 Previous', { 'keypress' : true });
+      return false;
+    }
+    if (e.keyCode === 39) {
+      closeModals();
+      flipsnap.toNext();
+      //mixpanel.track('YiR/2013 Next', { 'keypress' : true });
+      return false;
+    }
+    if(e.keyCode === 38 || e.keyCode === 40){
+      e.preventDefault();
+      closeModals();
+      toggleMenu();
+    }
+  });
+
+  if($('#flipsnap-controls').length && slideshowID == "team-slideshow"){
+    $('#flipsnap-controls li').click(function(){
+      
+      var myPosition = $(this).parent().children().index(this);
+
+      $('#flipsnap-controls li').removeClass('currentToggle');
+      $(this).addClass('currentToggle');
+
+      flipsnap.moveToPoint(myPosition);
+
+    })
+  }
+
+  console.log(slideshowID);
+
+
+  });
+
+  
+
+});
